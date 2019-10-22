dc5ed6e5515f1da1df511eb8a88d65d2229f5d7a
Bishesh Singh Ujjain <BU061787@cerner.com>
2019-10-22 17:30:36 +0530

    Integrating implementation for user defined winning score

--- application/app.js
+++ application/app.js
@@ -1,5 +1,6 @@
 var scores, roundScore, activePlayer, gamePlaying;
 
+var prev;
 //Initialize Game to default status
 init();
 
@@ -12,6 +13,7 @@ function nextPlayer() {
     document.querySelector('.player-1-panel').classList.toggle('active', false);
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
+    prev = 0;
 
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
@@ -32,10 +34,17 @@ document.querySelector('#rollDice').addEventListener('click', function() {
     diceDOM.src = 'images/' + 'dice-' + dice + '.png'; 
     
     //player looses his ENTIRE score when he rolls two 6 in a row.
-
+    
     if(dice !==1) {
+        if(dice === 6 && prev === 6) {
+            //choose next player.
+            nextPlayer();
+            prev = dice;
+        } else {
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        prev = dice;
+        }
     } else {
         //choose next player.
         nextPlayer();
@@ -52,7 +61,7 @@ document.querySelector('#hold').addEventListener('click', function() {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         //Check if player won the game
-        if(scores[activePlayer]> 20 ) {
+        if(scores[activePlayer]>= document.getElementById('SCORE_INPUT').value) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -72,6 +81,7 @@ function init() {
     roundScore = 0; 
     activePlayer = 0;
     gamePlaying = true;
+    prev = 0;
 
     //Dice img set to none
     document.querySelector('.dice').style.display = 'none';
